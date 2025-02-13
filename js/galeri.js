80acfa836f1a2e19b95f5f389eb7671d50b44232
Bekir <dedyusuf99@gmail.com>
2025-02-13 18:46:18 +0300

    Update galeri.js

--- js/galeri.js
+++ js/galeri.js
@@ -11,8 +11,17 @@ const images = [
     "images/image9.png",
     "images/image10.png",
     "images/image11.png",
-    "images/image12.png"
+    "images/image12.png",
+    "images/image13.png",
+    "images/image14.png",
+    "images/image15.png",
+    "images/image16.png",
+    "images/image17.png",
+    "images/image18.png",
+    "images/image19.png",
+    "images/image20.png"
 ];
+
 
 // Işıklı kutuyu açma fonksiyonu
 function openLightbox(index) {
